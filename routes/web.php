d64c2f85fbbb3499eedc3a7b74a930e6943c9e2d
Rashed Fahim Chowdhury <rashedfahimchowdhury@gmail.com>
2025-04-30 00:05:11 +0600

    home page updated

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,18 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+use App\Http\Controllers\DashboardController;
+
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/profile/games', [ProfileController::class, 'games'])->name('profile.games');
 });
 Route::get('/game/witcher3', function () {
     return view('games.witcher3');
